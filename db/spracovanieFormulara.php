b2e97dc4cb11fd9af06f3bc3971f006176d6c9ab
LiubymNaval <navallubim@gmail.com>
2024-04-23 00:16:35 +0200

    Commit5
    Pridanie odoslania formulára do databázy

---
+++ db/spracovanieFormulara.php
@@ -0,0 +1,41 @@
+<?php
+// PDO databázové pripojenie
+$host = "localhost";
+$dbname = "LOMBERX_FC";
+$port = 3307;
+$username = "root";
+$password = "";
+
+// Možnosti
+$options = array(
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+);
+
+// Pripojenie PDO
+try {
+    $conn = new PDO('mysql:host='.$host.';dbname='.$dbname.";port=".$port, $username,
+        $password, $options);
+} catch (PDOException $e) {
+    die("Chyba pripojenia: " . $e->getMessage());
+}
+// Získanie údajov z formulára
+$meno = $_POST["meno"];
+$email = $_POST["email"];
+$sprava = $_POST["sprava"];
+
+// SQL príkaz INSERT
+$sql = "INSERT INTO Formulary (Meno, Email, Sprava) 
+                VALUE ('".$meno."', '".$email."', '".$sprava."')";
+$statement = $conn->prepare($sql);
+try {
+    $insert = $statement->execute();
+    header("Location: http://localhost/SEMESTRALNY_PROJEKT/kontakt.php");
+    return $insert;
+} catch (\Exception $exception) {
+    return false;
+}
+// Zatvorenie pripojenia
+$conn = null;
+
+?>
